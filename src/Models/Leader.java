999d2fb5670268582ccc15f2708aef2c2b51c39f
Augustin <augustinbzmrmy@gmail.com>
2025-10-12 16:32:01 +0200

    Implement console-based move input validation

--- src/Models/Leader.java
+++ src/Models/Leader.java
@@ -1,4 +1,14 @@
 package Models;
 
-public class Leader {
+public class Leader extends Piece {
+    public Leader(int row, int col, String player) {
+        super("Leader", 'L', row, col, player);
+    }
+
+    @Override
+    public boolean canMove(int newRow, int newCol) {
+        int rowDiff = Math.abs(newRow - row);
+        int colDiff = Math.abs(newCol - col);
+        return rowDiff <= 1 && colDiff <= 1;
+    }
 }
--- src/Models/Piece.java
+++ src/Models/Piece.java
@@ -1,4 +1,23 @@
 package Models;
 
-public class Piece {
+public abstract class Piece {
+    protected String name;
+    protected char symbol;
+    protected int row;
+    protected int col;
+    protected String player;
+
+    public Piece(String name, char symbol, int row, int col, String player) {
+        this.name = name;
+        this.symbol = symbol;
+        this.row = row;
+        this.col = col;
+        this.player = player;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public abstract boolean canMove(int newRow, int newCol);
 }
